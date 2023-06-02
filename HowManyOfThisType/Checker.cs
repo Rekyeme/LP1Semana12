bde9b0300d18a36cfc1adb63d47409772ed36ba1
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-06-02 11:13:34 +0100

    Fixed the location of the Checker

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        public static int HowManyOfType <T> (Enumerable <Object> items)
+        {
+            int count = 0;
+            foreach (object item in items)
+            {
+                if (item is T)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
+}
